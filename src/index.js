b376aea33dd5eae91fd7fedc7e8f190eae6b5657
Daniel Lima <danielmlimag@gmail.com>
2023-06-10 23:00:50 -0300

    feat: implement 'order now' button functionality

--- src/index.js
+++ src/index.js
@@ -1,69 +1,72 @@
-import homeBackgroundImagePath from './assets/images/krusty-krab.jpg';
-import menuBackgroundImagePath from './assets/images/krusty-krab-inside.png';
-import contactBackgroundImagePath from './assets/images/bikini-bottom.jpg';
-import addHome from './pages/home/home';
-import addMenu from './pages/menu/menu';
-import addContact from './pages/contact/contact';
-import './reset.css';
-import './index.css';
+import homeBackgroundImagePath from "./assets/images/krusty-krab.jpg";
+import menuBackgroundImagePath from "./assets/images/krusty-krab-inside.png";
+import contactBackgroundImagePath from "./assets/images/bikini-bottom.jpg";
+import addHome from "./pages/home/home";
+import addMenu from "./pages/menu/menu";
+import addContact from "./pages/contact/contact";
+import "./reset.css";
+import "./index.css";
 
-const contentDiv = document.getElementById('content');
+const contentDiv = document.getElementById("content");
 
-const homeBackgroundImage = document.createElement('img');
+const homeBackgroundImage = document.createElement("img");
 homeBackgroundImage.src = homeBackgroundImagePath;
-homeBackgroundImage.className = 'background-image';
+homeBackgroundImage.className = "background-image";
 
 contentDiv.append(homeBackgroundImage);
 
-const navbar = document.createElement('header');
-navbar.className = 'navbar';
+const navbar = document.createElement("header");
+navbar.className = "navbar";
+navbar.id = "navbar";
 
-const nav = document.createElement('nav');
-nav.className = 'navbar__nav';
+const nav = document.createElement("nav");
+nav.className = "navbar__nav";
 
-const homeLink = document.createElement('a');
-homeLink.className = 'navbar__nav__item';
-homeLink.textContent = 'Home';
-
-homeLink.addEventListener('click', () => {
-	contentDiv.textContent = '';
-	homeLink.className = 'navbar__nav__item navbar__nav__item--active';
-	menuLink.className = contactLink.className = 'navbar__nav__item';
-	contentDiv.append(homeBackgroundImage);
-	contentDiv.append(navbar);
-	addHome();
+const homeLink = document.createElement("a");
+homeLink.className = "navbar__nav__item";
+homeLink.id = "home-link";
+homeLink.textContent = "Home";
+homeLink.addEventListener("click", () => {
+    contentDiv.textContent = "";
+    homeLink.className = "navbar__nav__item navbar__nav__item--active";
+    menuLink.className = contactLink.className = "navbar__nav__item";
+    contentDiv.append(homeBackgroundImage);
+    contentDiv.append(navbar);
+    addHome();
 });
 
-const menuBackgroundImage = document.createElement('img');
+const menuBackgroundImage = document.createElement("img");
 menuBackgroundImage.src = menuBackgroundImagePath;
-menuBackgroundImage.className = 'background-image';
+menuBackgroundImage.className = "background-image";
 
-const menuLink = document.createElement('a');
-menuLink.className = 'navbar__nav__item';
-menuLink.textContent = 'Menu';
-menuLink.addEventListener('click', () => {
-	contentDiv.textContent = '';
-	menuLink.className = 'navbar__nav__item navbar__nav__item--active';
-	homeLink.className = contactLink.className = 'navbar__nav__item';
-	contentDiv.append(menuBackgroundImage);
-	contentDiv.append(navbar);
-	addMenu();
+const menuLink = document.createElement("a");
+menuLink.className = "navbar__nav__item";
+menuLink.id = "menu-link";
+menuLink.textContent = "Menu";
+menuLink.addEventListener("click", () => {
+    contentDiv.textContent = "";
+    menuLink.className = "navbar__nav__item navbar__nav__item--active";
+    homeLink.className = contactLink.className = "navbar__nav__item";
+    contentDiv.append(menuBackgroundImage);
+    contentDiv.append(navbar);
+    addMenu();
 });
 
-const contactBackgroundImage = document.createElement('img');
+const contactBackgroundImage = document.createElement("img");
 contactBackgroundImage.src = contactBackgroundImagePath;
-contactBackgroundImage.className = 'background-image';
+contactBackgroundImage.className = "background-image";
 
-const contactLink = document.createElement('a');
-contactLink.className = 'navbar__nav__item';
-contactLink.textContent = 'Contact';
-contactLink.addEventListener('click', () => {
-	contentDiv.textContent = '';
-	contactLink.className = 'navbar__nav__item navbar__nav__item--active';
-	homeLink.className = menuLink.className = 'navbar__nav__item';
-	contentDiv.append(contactBackgroundImage);
-	contentDiv.append(navbar);
-	addContact();
+const contactLink = document.createElement("a");
+contactLink.className = "navbar__nav__item";
+contactLink.id = "contact-link";
+contactLink.textContent = "Contact";
+contactLink.addEventListener("click", () => {
+    contentDiv.textContent = "";
+    contactLink.className = "navbar__nav__item navbar__nav__item--active";
+    homeLink.className = menuLink.className = "navbar__nav__item";
+    contentDiv.append(contactBackgroundImage);
+    contentDiv.append(navbar);
+    addContact();
 });
 
 nav.append(homeLink, menuLink, contactLink);
@@ -71,5 +74,7 @@ nav.append(homeLink, menuLink, contactLink);
 navbar.append(nav);
 
 contentDiv.append(navbar);
-homeLink.className = 'navbar__nav__item navbar__nav__item--active';
+
+homeLink.className = "navbar__nav__item navbar__nav__item--active";
+
 addHome();
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,21 +1,38 @@
-import './home.css';
+import "./home.css";
+import addMenu from "../menu/menu.js";
+import menuBackgroundImagePath from "../../assets/images/krusty-krab-inside.png";
 
 function addHome() {
-	const container = document.createElement('div');
-	container.className = 'home';
+    const container = document.createElement("div");
+    container.className = "home";
 
-	const paragraph = document.createElement('h1');
-	paragraph.className = 'home__title'
-	paragraph.innerHTML = 'Come aboard the Krusty Krab';
-	
-	const button = document.createElement('button');
-	button.textContent = 'Order Now';
-	button.className = 'home__button';
+    const paragraph = document.createElement("h1");
+    paragraph.className = "home__title";
+    paragraph.innerHTML = "Come aboard the Krusty Krab";
 
-	container.append(paragraph, button);
+    const contentDiv = document.getElementById("content");
+    const menuLink = document.getElementById("menu-link");
+    const homeLink = document.getElementById("home-link");
+    const contactLink = document.getElementById("contact-link");
+    const menuBackgroundImage = document.createElement("img");
+    menuBackgroundImage.src = menuBackgroundImagePath;
+    menuBackgroundImage.className = "background-image";
+    const navbar = document.getElementById("navbar");
 
-	const contentDiv = document.getElementById('content');
-	contentDiv.append(container);
+    const button = document.createElement("button");
+    button.textContent = "Order Now";
+    button.className = "home__button";
+    button.addEventListener("click", () => {
+        contentDiv.textContent = "";
+        menuLink.className = "navbar__nav__item navbar__nav__item--active";
+        homeLink.className = contactLink.className = "navbar__nav__item";
+        contentDiv.append(menuBackgroundImage);
+        contentDiv.append(navbar);
+        addMenu();
+    });
+
+    container.append(paragraph, button);
+    contentDiv.append(container);
 }
 
 export default addHome;
